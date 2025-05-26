d28bf8551eeab02bf576b149d6fff76c5237efa0
PaulieB14 <94752445+PaulieB14@users.noreply.github.com>
2025-05-25 20:51:08 -0400

    Fix Apollo Client setup with proper configuration

--- src/index.js
+++ src/index.js
@@ -2,10 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+// Configure Apollo Client for The Graph
+// Using the hosted service endpoint
+const client = new ApolloClient({
+  uri: 'https://api.thegraph.com/subgraphs/id/QmdyCguLEisTtQFveEkvMhTH7UzjyhnrF9kpvhYeG4QX8a',
+  cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'cache-and-network',
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
