bf8334f6e7ab3c1c4a30a844782e13b04681ef97
PaulieB14 <94752445+PaulieB14@users.noreply.github.com>
2025-05-25 20:36:28 -0400

    Add Tailwind and PostCSS config

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          750: '#2d3748',
+          850: '#1a202c',
+          950: '#0f1419'
+        }
+      },
+      animation: {
+        'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      }
+    },
+  },
+  plugins: [],
+}
