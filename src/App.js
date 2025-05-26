484c821a32c5d46ebda815616909e143970e2f01
PaulieB14 <94752445+PaulieB14@users.noreply.github.com>
2025-05-26 19:03:48 -0400

    Update App.js to use Apollo Provider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { ApolloProvider } from '@apollo/client';
 import Dashboard from './components/Dashboard';
+import client from './apollo-client';
 
 function App() {
   return (
-    <div className="App">
-      <Dashboard />
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <Dashboard />
+      </div>
+    </ApolloProvider>
   );
 }
 
