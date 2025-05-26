57927c5db29d7a76b2c669ed99a51e7adce9c99f
PaulieB14 <94752445+PaulieB14@users.noreply.github.com>
2025-05-26 16:51:12 -0400

    Remove Apollo Client since we're using Polymarket API directly

--- src/index.js
+++ src/index.js
@@ -2,59 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
-
-// Configure Apollo Client for The Graph
-const httpLink = createHttpLink({
-  uri: 'https://gateway-arbitrum.network.thegraph.com/api/subgraphs/id/QmdyCguLEisTtQFveEkvMhTH7UzjyhnrF9kpvhYeG4QX8a',
-  fetch: (...args) => fetch(...args),
-});
-
-const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache({
-    typePolicies: {
-      Query: {
-        fields: {
-          global: {
-            merge: true,
-          },
-        },
-      },
-    },
-  }),
-  defaultOptions: {
-    watchQuery: {
-      fetchPolicy: 'network-only',
-      errorPolicy: 'all',
-    },
-    query: {
-      fetchPolicy: 'network-only',
-      errorPolicy: 'all',
-    },
-  },
-});
-
-// Test the connection
-client.query({
-  query: `
-    query TestConnection {
-      global(id: "") {
-        id
-      }
-    }
-  `
-}).then(result => {
-  console.log('GraphQL connection successful:', result);
-}).catch(error => {
-  console.error('GraphQL connection error:', error);
-});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <App />
   </React.StrictMode>
 );
